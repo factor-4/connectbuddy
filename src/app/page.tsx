b1b6aa387e48d9f21d97d7d8eb6218e336320572
Shahn Khan <snk.sojib@gmail.com>
2024-02-14 19:25:19 +0200

    sign in sign out pages added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="p-10">
-      <UserButton />
+      <UserButton  afterSignOutUrl="/sign-in"/>
     </div>
   );
 }
