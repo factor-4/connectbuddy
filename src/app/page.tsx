6958df00f06a7951731ff779f237492e1f990e76
Shahn Khan <snk.sojib@gmail.com>
2024-02-17 17:21:34 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,22 +6,24 @@ export default async function Home() {
   console.log('loggedinuser ', loggedInUserData)
 
 
-  
+
   return (
     <div className="p-10">
 
       <div className="flex flex-col gap-3 text-3xl">
         <UserButton afterSignOutUrl="/s text-3xlin" />
-        <span>First Name: 
+        <span>First Name:
           {loggedInUserData?.firstName}
         </span>
-        <span>Last Name : 
-        {loggedInUserData?.lastName}
+        <span>Last Name :
+          {loggedInUserData?.lastName}
         </span>
         <span>Username :
-        {loggedInUserData?.username}
+          {loggedInUserData?.username}
         </span>
-        <span>Email</span>
+        <span>Email
+          {loggedInUserData?.username}
+        </span>
       </div>
     </div>
   );
